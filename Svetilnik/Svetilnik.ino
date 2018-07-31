fdf95b4d0a789052a5ab74a7ab1eaba116346742
AlMaz <almaz8811@gmail.com>
2018-08-01 00:41:39 +0300

    Add files via upload

---
+++ Svetilnik/Svetilnik.ino
@@ -0,0 +1,519 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <Adafruit_NeoPixel.h>
+
+// Установка порта, количества светодиодов и яркости
+#define PIN            D7
+#define NUMPIXELS      1
+#define BRIGHTNESS     20
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+const char* ssid = "QtechRouter-115e";
+const char* password = "Q6K7TGR5";
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "31f94ba459c14d2bbd24b8c90244aae7";
+char server[] = "139.59.206.133";
+char port[] = "8441";
+// Your WiFi credentials.
+// Set password to "" for open networks.
+//char ssid[] = "YourNetworkName";
+//char pass[] = "YourPassword";
+int effect = 1;
+
+
+
+//BLYNK_WRITE(V3) {
+//  switch (param.asInt()) {
+//    case 1: {
+//      f = "111";
+//        break;
+//      }
+//    }
+//  }
+
+//BLYNK_READ(V1)
+//{
+//  Blynk.virtualWrite(V1, f);
+//}
+
+int gamma1[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
+
+    
+int delayval = 50;
+int r,g,b,j  = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Blynk.config(auth);
+  Blynk.disconnect();
+  Blynk.connect();
+  strip.begin(); // This initializes the NeoPixel library.
+}
+
+BLYNK_WRITE(V6) {
+  switch (param.asInt()) {
+    case 1: {
+      effect = 1;
+      break;
+      }
+      case 2: {
+      effect = 2;
+      break;
+      }
+      case 3: {
+      effect = 3;
+      break;
+      }
+      case 4: {
+      effect = 4;
+      break;
+      }
+      case 5: {
+      effect = 5;
+      break;
+      }
+      case 6: {
+      effect = 6;
+      break;
+      }
+      case 7: {
+      effect = 7;
+      break;
+      }
+      case 8: {
+      effect = 8;
+      break;
+      }
+      case 9: {
+      effect = 9;
+      break;
+      }
+      case 10: {
+      effect = 10;
+      break;
+      }
+    }
+  }
+
+  
+void loop() {
+
+
+  ArduinoOTA.handle();
+  Blynk.run();
+
+if (effect == 1) {
+  // Первый эффект
+   if (j == 1) {
+     r = 0;
+     g = 150;
+     b = 0;
+  } else if (j == 2) {
+     r = 150;
+     g = 0;
+     b = 0;
+  } else {
+     r = 0;
+     g = 0;
+     b = 150;
+     j = 0;
+  }
+  j++;
+
+  for(int i=0;i<NUMPIXELS;i++){
+
+    // strip.Color takes RGB values, from 0,0,0 up to 255,255,255
+    strip.setPixelColor(i, strip.Color(r,g,b)); // Moderately bright green color.
+
+    strip.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+  }
+} else if (effect == 2) {
+  //Второй эффект
+  rainbowCycle(2);
+} else if (effect == 3) {
+    colorWipe(strip.Color(0, 0, 0), 50);    // Black/off
+  colorWipe(strip.Color(255, 0, 0), 50);  // Red
+  colorWipe(strip.Color(0, 255, 0), 50);  // Green
+  colorWipe(strip.Color(0, 0, 255), 50);  // Blue
+} else if (effect == 4) {
+    theaterChase(strip.Color(127, 127, 127), 50); // White
+  theaterChase(strip.Color(127,   0,   0), 50); // Red
+  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+} else if (effect == 5) {
+  rainbow(20);
+} else if (effect == 6) {
+  theaterChaseRainbow(50);
+} else if (effect == 7) {
+  whiteOverRainbow(20,75,8);
+} else if (effect == 8) {
+  pulseWhite(5);
+} else if (effect == 9) {
+  fullWhite();
+  delay(100);
+} else if (effect == 10) {
+  rainbowFade2White(3,3,1);
+}
+  
+// if (j == 1) {
+//     r = 0;
+//     g = 150;
+//     b = 0;
+//  } else if (j == 2) {
+//     r = 150;
+//     g = 0;
+//     b = 0;
+//  } else {
+//     r = 0;
+//     g = 0;
+//     b = 150;
+//     j = 0;
+//  }
+//  j++;
+//
+//  for(int i=0;i<NUMPIXELS;i++){
+//
+//    // strip.Color takes RGB values, from 0,0,0 up to 255,255,255
+//    strip.setPixelColor(i, strip.Color(r,g,b)); // Moderately bright green color.
+//
+//    strip.show(); // This sends the updated pixel color to the hardware.
+//
+//    delay(delayval); // Delay for a period of time (in milliseconds).
+//
+//  }
+
+//  rainbowCycle(2);
+
+//  colorWipe(strip.Color(0, 0, 0), 50);    // Black/off
+//  colorWipe(strip.Color(255, 0, 0), 50);  // Red
+//  colorWipe(strip.Color(0, 255, 0), 50);  // Green
+//  colorWipe(strip.Color(0, 0, 255), 50);  // Blue
+
+//  theaterChase(strip.Color(127, 127, 127), 50); // White
+//  theaterChase(strip.Color(127,   0,   0), 50); // Red
+//  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+
+//  rainbow(20);
+
+//  theaterChaseRainbow(50);
+
+//  whiteOverRainbow(20,75,8);  
+
+//  pulseWhite(5); 
+
+//  fullWhite();
+//  delay(100);
+
+//  rainbowFade2White(3,3,1);
+
+}
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+void pulseWhite(uint8_t wait) {
+  for(int j = 0; j < 256 ; j++){
+      for(uint16_t i=0; i<strip.numPixels(); i++) {
+          strip.setPixelColor(i, strip.Color(0,0,0, gamma1[j] ) );
+        }
+        delay(wait);
+        strip.show();
+      }
+
+  for(int j = 255; j >= 0 ; j--){
+      for(uint16_t i=0; i<strip.numPixels(); i++) {
+          strip.setPixelColor(i, strip.Color(0,0,0, gamma1[j] ) );
+        }
+        delay(wait);
+        strip.show();
+      }
+}
+
+
+void rainbowFade2White(uint8_t wait, int rainbowLoops, int whiteLoops) {
+  float fadeMax = 100.0;
+  int fadeVal = 0;
+  uint32_t wheelVal;
+  int redVal, greenVal, blueVal;
+
+  for(int k = 0 ; k < rainbowLoops ; k ++){
+    
+    for(int j=0; j<256; j++) { // 5 cycles of all colors on wheel
+
+      for(int i=0; i< strip.numPixels(); i++) {
+
+        wheelVal = Wheel(((i * 256 / strip.numPixels()) + j) & 255);
+
+        redVal = red(wheelVal) * float(fadeVal/fadeMax);
+        greenVal = green(wheelVal) * float(fadeVal/fadeMax);
+        blueVal = blue(wheelVal) * float(fadeVal/fadeMax);
+
+        strip.setPixelColor( i, strip.Color( redVal, greenVal, blueVal ) );
+
+      }
+
+      //First loop, fade in!
+      if(k == 0 && fadeVal < fadeMax-1) {
+          fadeVal++;
+      }
+
+      //Last loop, fade out!
+      else if(k == rainbowLoops - 1 && j > 255 - fadeMax ){
+          fadeVal--;
+      }
+
+        strip.show();
+        delay(wait);
+    }
+  
+  }
+
+
+
+  delay(500);
+
+
+  for(int k = 0 ; k < whiteLoops ; k ++){
+
+    for(int j = 0; j < 256 ; j++){
+
+        for(uint16_t i=0; i < strip.numPixels(); i++) {
+            strip.setPixelColor(i, strip.Color(0,0,0, gamma1[j] ) );
+          }
+          strip.show();
+        }
+
+        delay(2000);
+    for(int j = 255; j >= 0 ; j--){
+
+        for(uint16_t i=0; i < strip.numPixels(); i++) {
+            strip.setPixelColor(i, strip.Color(0,0,0, gamma1[j] ) );
+          }
+          strip.show();
+        }
+  }
+
+  delay(500);
+
+
+}
+
+void whiteOverRainbow(uint8_t wait, uint8_t whiteSpeed, uint8_t whiteLength ) {
+  
+  if(whiteLength >= strip.numPixels()) whiteLength = strip.numPixels() - 1;
+
+  int head = whiteLength - 1;
+  int tail = 0;
+
+  int loops = 3;
+  int loopNum = 0;
+
+  static unsigned long lastTime = 0;
+
+
+  while(true){
+    for(int j=0; j<256; j++) {
+      for(uint16_t i=0; i<strip.numPixels(); i++) {
+        if((i >= tail && i <= head) || (tail > head && i >= tail) || (tail > head && i <= head) ){
+          strip.setPixelColor(i, strip.Color(0,0,0, 255 ) );
+        }
+        else{
+          strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+        }
+        
+      }
+
+      if(millis() - lastTime > whiteSpeed) {
+        head++;
+        tail++;
+        if(head == strip.numPixels()){
+          loopNum++;
+        }
+        lastTime = millis();
+      }
+
+      if(loopNum == loops) return;
+    
+      head%=strip.numPixels();
+      tail%=strip.numPixels();
+        strip.show();
+        delay(wait);
+    }
+  }
+  
+}
+void fullWhite() {
+  
+    for(uint16_t i=0; i<strip.numPixels(); i++) {
+        strip.setPixelColor(i, strip.Color(0,0,0, 255 ) );
+    }
+      strip.show();
+}
+
+
+uint8_t red(uint32_t c) {
+  return (c >> 8);
+}
+uint8_t green(uint32_t c) {
+  return (c >> 16);
+}
+uint8_t blue(uint32_t c) {
+  return (c);
+}
+
